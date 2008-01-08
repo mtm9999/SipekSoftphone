5268e70ef809e40ab64a322d44b6b5a54709c57f
sasacoh <sasacoh@30a9d3e4-4a32-0410-933f-db000b6b029f>
2008-01-08 11:35:15 +0000

    GUI Project file update & Version correction

--- trunk/phone/Properties/AssemblyInfo.cs
+++ trunk/phone/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.35.114")]
+[assembly: AssemblyVersion("0.0.36.114")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
