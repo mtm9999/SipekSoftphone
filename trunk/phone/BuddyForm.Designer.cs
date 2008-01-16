16dc0fafe171ea44cf3e3b7ddc7d0159ff46034f
sasacoh <sasacoh@30a9d3e4-4a32-0410-933f-db000b6b029f>
2008-01-16 13:50:16 +0000

    Minor updates

--- trunk/phone/BuddyForm.Designer.cs
+++ trunk/phone/BuddyForm.Designer.cs
@@ -29,19 +29,21 @@ namespace Sipek
     private void InitializeComponent()
     {
       this.groupBox1 = new System.Windows.Forms.GroupBox();
-      this.textBoxName = new System.Windows.Forms.TextBox();
-      this.label1 = new System.Windows.Forms.Label();
+      this.buttonOk = new System.Windows.Forms.Button();
+      this.buttonCancel = new System.Windows.Forms.Button();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.label3 = new System.Windows.Forms.Label();
       this.label2 = new System.Windows.Forms.Label();
       this.textBoxNumber = new System.Windows.Forms.TextBox();
-      this.label3 = new System.Windows.Forms.Label();
-      this.comboBox1 = new System.Windows.Forms.ComboBox();
-      this.buttonCancel = new System.Windows.Forms.Button();
-      this.buttonOk = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBoxName = new System.Windows.Forms.TextBox();
+      this.checkBox1 = new System.Windows.Forms.CheckBox();
       this.groupBox1.SuspendLayout();
       this.SuspendLayout();
       // 
       // groupBox1
       // 
+      this.groupBox1.Controls.Add(this.checkBox1);
       this.groupBox1.Controls.Add(this.buttonOk);
       this.groupBox1.Controls.Add(this.buttonCancel);
       this.groupBox1.Controls.Add(this.comboBox1);
@@ -53,74 +55,13 @@ namespace Sipek
       this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.groupBox1.Location = new System.Drawing.Point(0, 0);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(292, 266);
+      this.groupBox1.Size = new System.Drawing.Size(247, 194);
       this.groupBox1.TabIndex = 0;
       this.groupBox1.TabStop = false;
-      this.groupBox1.Text = "Settings";
-      // 
-      // textBoxName
-      // 
-      this.textBoxName.Location = new System.Drawing.Point(93, 36);
-      this.textBoxName.Name = "textBoxName";
-      this.textBoxName.Size = new System.Drawing.Size(187, 20);
-      this.textBoxName.TabIndex = 0;
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(13, 42);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(72, 13);
-      this.label1.TabIndex = 1;
-      this.label1.Text = "Display Name";
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(13, 68);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(45, 13);
-      this.label2.TabIndex = 3;
-      this.label2.Text = "Address";
-      // 
-      // textBoxNumber
-      // 
-      this.textBoxNumber.Location = new System.Drawing.Point(93, 62);
-      this.textBoxNumber.Name = "textBoxNumber";
-      this.textBoxNumber.Size = new System.Drawing.Size(146, 20);
-      this.textBoxNumber.TabIndex = 2;
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(13, 123);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(41, 13);
-      this.label3.TabIndex = 5;
-      this.label3.Text = "Melody";
-      // 
-      // comboBox1
-      // 
-      this.comboBox1.FormattingEnabled = true;
-      this.comboBox1.Location = new System.Drawing.Point(93, 123);
-      this.comboBox1.Name = "comboBox1";
-      this.comboBox1.Size = new System.Drawing.Size(121, 21);
-      this.comboBox1.TabIndex = 6;
-      // 
-      // buttonCancel
-      // 
-      this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.buttonCancel.Location = new System.Drawing.Point(30, 231);
-      this.buttonCancel.Name = "buttonCancel";
-      this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-      this.buttonCancel.TabIndex = 7;
-      this.buttonCancel.Text = "Cancel";
-      this.buttonCancel.UseVisualStyleBackColor = true;
-      this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
       // 
       // buttonOk
       // 
-      this.buttonOk.Location = new System.Drawing.Point(180, 231);
+      this.buttonOk.Location = new System.Drawing.Point(139, 159);
       this.buttonOk.Name = "buttonOk";
       this.buttonOk.Size = new System.Drawing.Size(75, 23);
       this.buttonOk.TabIndex = 8;
@@ -128,13 +69,83 @@ namespace Sipek
       this.buttonOk.UseVisualStyleBackColor = true;
       this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
       // 
+      // buttonCancel
+      // 
+      this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.buttonCancel.Location = new System.Drawing.Point(16, 159);
+      this.buttonCancel.Name = "buttonCancel";
+      this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+      this.buttonCancel.TabIndex = 7;
+      this.buttonCancel.Text = "Cancel";
+      this.buttonCancel.UseVisualStyleBackColor = true;
+      this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(93, 85);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(121, 21);
+      this.comboBox1.TabIndex = 6;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(44, 88);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(41, 13);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Melody";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(40, 62);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(45, 13);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Address";
+      // 
+      // textBoxNumber
+      // 
+      this.textBoxNumber.Location = new System.Drawing.Point(93, 59);
+      this.textBoxNumber.Name = "textBoxNumber";
+      this.textBoxNumber.Size = new System.Drawing.Size(121, 20);
+      this.textBoxNumber.TabIndex = 2;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(13, 36);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(72, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Display Name";
+      // 
+      // textBoxName
+      // 
+      this.textBoxName.Location = new System.Drawing.Point(93, 33);
+      this.textBoxName.Name = "textBoxName";
+      this.textBoxName.Size = new System.Drawing.Size(121, 20);
+      this.textBoxName.TabIndex = 0;
+      // 
+      // checkBox1
+      // 
+      this.checkBox1.AutoSize = true;
+      this.checkBox1.Location = new System.Drawing.Point(93, 113);
+      this.checkBox1.Name = "checkBox1";
+      this.checkBox1.Size = new System.Drawing.Size(71, 17);
+      this.checkBox1.TabIndex = 9;
+      this.checkBox1.Text = "Presence";
+      this.checkBox1.UseVisualStyleBackColor = true;
+      // 
       // BuddyForm
       // 
       this.AcceptButton = this.buttonOk;
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this.buttonCancel;
-      this.ClientSize = new System.Drawing.Size(292, 266);
+      this.ClientSize = new System.Drawing.Size(247, 194);
       this.Controls.Add(this.groupBox1);
       this.Name = "BuddyForm";
       this.Text = "Buddy Room";
@@ -156,5 +167,6 @@ namespace Sipek
     private System.Windows.Forms.Label label3;
     private System.Windows.Forms.Label label2;
     private System.Windows.Forms.TextBox textBoxNumber;
+    private System.Windows.Forms.CheckBox checkBox1;
   }
 }
--- trunk/phone/SettingsForm.cs
+++ trunk/phone/SettingsForm.cs
@@ -1,3 +1,24 @@
+/* 
+ * Copyright (C) 2008 Sasa Coh <sasacoh@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ * 
+ * WaveLib library sources http://www.codeproject.com/KB/graphics/AudioLib.aspx
+ * 
+ */
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,7 +27,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using Telephony;
-using WaveLib.AudioMixer;
+using WaveLib.AudioMixer; // see http://www.codeproject.com/KB/graphics/AudioLib.aspx
 
 namespace Sipek
 {
@@ -528,5 +549,10 @@ namespace Sipek
       }
     }
 
+
+    public void activateTab(int index)
+    {
+      this.tabControlSettings.SelectTab(index);
+    }
   }
 }
