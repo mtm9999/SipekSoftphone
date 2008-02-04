f12bc932c1d0234cc7c4882712aa343e1eda9ccf
sasacoh <sasacoh@30a9d3e4-4a32-0410-933f-db000b6b029f>
2008-02-04 07:17:14 +0000

    Redesign: Modularization (Telephony module)

--- phone/KeyboardForm.Designer.cs
+++ phone/KeyboardForm.Designer.cs
@@ -66,133 +66,133 @@ namespace Sipek
       // 
       // keypad0
       // 
+      this.keypad0.Image = global::Sipek.Properties.Resources.DialPad0;
       this.keypad0.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad0.Location = new System.Drawing.Point(48, 120);
       this.keypad0.Name = "keypad0";
       this.keypad0.Size = new System.Drawing.Size(30, 30);
       this.keypad0.TabIndex = 34;
-      this.keypad0.Text = "0";
       this.keypad0.UseVisualStyleBackColor = true;
       this.keypad0.Click += new System.EventHandler(this.keypad0_Click);
       // 
       // keypad1
       // 
+      this.keypad1.Image = global::Sipek.Properties.Resources.DialPad1;
       this.keypad1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad1.Location = new System.Drawing.Point(12, 84);
       this.keypad1.Name = "keypad1";
       this.keypad1.Size = new System.Drawing.Size(30, 30);
       this.keypad1.TabIndex = 30;
-      this.keypad1.Text = "1";
       this.keypad1.UseVisualStyleBackColor = true;
       this.keypad1.Click += new System.EventHandler(this.keypad1_Click);
       // 
       // keypad2
       // 
+      this.keypad2.Image = global::Sipek.Properties.Resources.DialPad2;
       this.keypad2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad2.Location = new System.Drawing.Point(48, 84);
       this.keypad2.Name = "keypad2";
       this.keypad2.Size = new System.Drawing.Size(30, 30);
       this.keypad2.TabIndex = 31;
-      this.keypad2.Text = "2";
       this.keypad2.UseVisualStyleBackColor = true;
       this.keypad2.Click += new System.EventHandler(this.keypad2_Click);
       // 
       // keypad3
       // 
+      this.keypad3.Image = global::Sipek.Properties.Resources.DialPad3;
       this.keypad3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad3.Location = new System.Drawing.Point(84, 84);
       this.keypad3.Name = "keypad3";
       this.keypad3.Size = new System.Drawing.Size(30, 30);
       this.keypad3.TabIndex = 32;
-      this.keypad3.Text = "3";
       this.keypad3.UseVisualStyleBackColor = true;
       this.keypad3.Click += new System.EventHandler(this.keypad3_Click);
       // 
       // keypad4
       // 
+      this.keypad4.Image = global::Sipek.Properties.Resources.DialPad4;
       this.keypad4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad4.Location = new System.Drawing.Point(12, 48);
       this.keypad4.Name = "keypad4";
       this.keypad4.Size = new System.Drawing.Size(30, 30);
       this.keypad4.TabIndex = 27;
-      this.keypad4.Text = "4";
       this.keypad4.UseVisualStyleBackColor = true;
       this.keypad4.Click += new System.EventHandler(this.keypad4_Click);
       // 
       // keypad5
       // 
+      this.keypad5.Image = global::Sipek.Properties.Resources.DialPad5;
       this.keypad5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad5.Location = new System.Drawing.Point(48, 48);
       this.keypad5.Name = "keypad5";
       this.keypad5.Size = new System.Drawing.Size(30, 30);
       this.keypad5.TabIndex = 28;
-      this.keypad5.Text = "5";
       this.keypad5.UseVisualStyleBackColor = true;
       this.keypad5.Click += new System.EventHandler(this.keypad5_Click);
       // 
       // keypad6
       // 
+      this.keypad6.Image = global::Sipek.Properties.Resources.DialPad6;
       this.keypad6.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad6.Location = new System.Drawing.Point(84, 48);
       this.keypad6.Name = "keypad6";
       this.keypad6.Size = new System.Drawing.Size(30, 30);
       this.keypad6.TabIndex = 29;
-      this.keypad6.Text = "6";
       this.keypad6.UseVisualStyleBackColor = true;
       this.keypad6.Click += new System.EventHandler(this.keypad6_Click);
       // 
       // keypad7
       // 
+      this.keypad7.Image = global::Sipek.Properties.Resources.DialPad7;
       this.keypad7.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad7.Location = new System.Drawing.Point(12, 12);
       this.keypad7.Name = "keypad7";
       this.keypad7.Size = new System.Drawing.Size(30, 30);
       this.keypad7.TabIndex = 24;
-      this.keypad7.Text = "7";
       this.keypad7.UseVisualStyleBackColor = true;
       this.keypad7.Click += new System.EventHandler(this.keypad7_Click);
       // 
       // keypad8
       // 
+      this.keypad8.Image = global::Sipek.Properties.Resources.DialPad8;
       this.keypad8.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad8.Location = new System.Drawing.Point(48, 12);
       this.keypad8.Name = "keypad8";
       this.keypad8.Size = new System.Drawing.Size(30, 30);
       this.keypad8.TabIndex = 25;
-      this.keypad8.Text = "8";
       this.keypad8.UseVisualStyleBackColor = true;
       this.keypad8.Click += new System.EventHandler(this.keypad8_Click);
       // 
       // keypad9
       // 
+      this.keypad9.Image = global::Sipek.Properties.Resources.DialPad9;
       this.keypad9.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypad9.Location = new System.Drawing.Point(84, 12);
       this.keypad9.Name = "keypad9";
       this.keypad9.Size = new System.Drawing.Size(30, 30);
       this.keypad9.TabIndex = 26;
-      this.keypad9.Text = "9";
       this.keypad9.UseVisualStyleBackColor = true;
       this.keypad9.Click += new System.EventHandler(this.keypad9_Click);
       // 
       // keypadHASH
       // 
+      this.keypadHASH.Image = global::Sipek.Properties.Resources.DialPadHash;
       this.keypadHASH.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypadHASH.Location = new System.Drawing.Point(84, 120);
       this.keypadHASH.Name = "keypadHASH";
       this.keypadHASH.Size = new System.Drawing.Size(30, 30);
       this.keypadHASH.TabIndex = 35;
-      this.keypadHASH.Text = "#";
       this.keypadHASH.UseVisualStyleBackColor = true;
       this.keypadHASH.Click += new System.EventHandler(this.keypadHASH_Click);
       // 
       // keypadSTAR
       // 
+      this.keypadSTAR.Image = global::Sipek.Properties.Resources.DialPadStar;
       this.keypadSTAR.ImeMode = System.Windows.Forms.ImeMode.NoControl;
       this.keypadSTAR.Location = new System.Drawing.Point(12, 120);
       this.keypadSTAR.Name = "keypadSTAR";
       this.keypadSTAR.Size = new System.Drawing.Size(30, 30);
       this.keypadSTAR.TabIndex = 33;
-      this.keypadSTAR.Text = "*";
       this.keypadSTAR.UseVisualStyleBackColor = true;
       this.keypadSTAR.Click += new System.EventHandler(this.keypadSTAR_Click);
       // 
---
+++ pjsipWrapper/callProxy.cs
@@ -0,0 +1,447 @@
+/* 
+ * Copyright (C) 2007 Sasa Coh <sasacoh@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+using System.Runtime.InteropServices;
+using System.Threading;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using Telephony;
+
+namespace PjsipWrapper
+{
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public class CSipCallProxy : ICallProxyInterface
+  {
+    #region DLL declarations
+    // call API
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_makeCall(int accountId, string uri);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_releaseCall(int callId);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_answerCall(int callId, int code);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_holdCall(int callId);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_retrieveCall(int callId);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_xferCall(int callId, string uri);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_xferCallWithReplaces(int callId, int dstSession);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_serviceReq(int callId, int serviceCode, string destUri);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_dialDtmf(int callId, string digits, int mode);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_sendInfo(int callid, string content);
+
+    #endregion
+
+    #region Properties
+    private CCallManager Manager
+    {
+      get { return CCallManager.getInstance(); }
+    }
+    #endregion
+
+    #region Constructor
+
+    #endregion Constructor
+
+
+    #region Methods
+
+    /// <summary>
+    /// Method makeCall creates call session
+    /// </summary>
+    /// <param name="dialedNo"></param>
+    /// <param name="accountId"></param>
+    /// <returns>SessionId selected by sip stack</returns>
+    public int makeCall(string dialedNo, int accountId)
+    {
+      string uri = "sip:" + dialedNo + "@" + Manager.Config.getAccount(accountId).HostName;
+      int sessionId = dll_makeCall(accountId, uri);
+      return sessionId;
+    }
+
+    public bool endCall(int sessionId)
+    {
+      dll_releaseCall(sessionId);
+      return true;
+    }
+
+    public bool alerted(int sessionId)
+    {
+      dll_answerCall(sessionId, 180);
+      return true;
+    }
+
+    public bool acceptCall(int sessionId)
+    {
+      dll_answerCall(sessionId, 200);
+      return true;
+    }
+    
+    public bool holdCall(int sessionId)
+    {
+      dll_holdCall(sessionId);
+      return true;
+    }
+    
+    public bool retrieveCall(int sessionId)
+    {
+      dll_retrieveCall(sessionId);
+      return true;
+    }
+          
+    public bool xferCall(int sessionId, string number)
+    {
+      string uri = "sip:" + number + "@" + Manager.Config.getAccount().HostName;
+      dll_xferCall(sessionId, uri);
+      return true;
+    }
+    
+    public bool xferCallSession(int sessionId, int session)
+    {
+      dll_xferCallWithReplaces(sessionId, session);
+      return true;
+    }
+
+    public bool threePtyCall(int sessionId, int session)
+    {
+      dll_serviceReq(sessionId, (int)EServiceCodes.SC_3PTY, "");
+      return true;
+    }
+    
+    public bool serviceRequest(int sessionId, int code, string dest)
+    {
+      string destUri = "<sip:" + dest + "@" + Manager.Config.getAccount().HostName + ">";
+      dll_serviceReq(sessionId, (int)code, destUri);
+      return true;
+    }
+
+    public bool dialDtmf(int sessionId, string digits, int mode)
+    {
+      // todo:::check the dtmf mode
+      if (mode == 0)
+      {
+        dll_dialDtmf(sessionId, digits, mode);
+      }
+      else
+      {
+        dll_sendInfo(sessionId, digits);
+      }
+      return true;
+    }
+
+    #endregion Methods
+
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public class CSipCommonProxy : ICommonProxyInterface
+  {  
+
+    #region Wrapper functions
+    // callback delegates
+    delegate int GetConfigData(int cfgId);
+    delegate int OnRegStateChanged(int accountId, int regState);
+    delegate int OnCallStateChanged(int callId, int stateId);
+    delegate int OnCallIncoming(int callId, string number);
+    delegate int OnCallHoldConfirm(int callId);
+    delegate int OnMessageReceivedCallback(string from, string message);
+    delegate int OnBuddyStatusChangedCallback(int buddyId, int status, string statusText);
+
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_init(int listenPort);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_main();
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_shutdown();
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_registerAccount(string uri, string reguri, string domain, string username, string password);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_addBuddy(string uri, bool subscribe);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_removeBuddy(int buddyId);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_sendMessage(int buddyId, string uri, string message);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_setStatus(int accId, int presence_state);
+    [DllImport("pjsipDll.dll")]
+    private static extern int dll_removeAccounts();
+
+    // call API callbacks
+    [DllImport("pjsipDll.dll")]
+    private static extern int onCallStateCallback(OnCallStateChanged cb);
+    [DllImport("pjsipDll.dll")]
+    private static extern int onRegStateCallback(OnRegStateChanged cb);
+    [DllImport("pjsipDll.dll")]
+    private static extern int onCallIncoming(OnCallIncoming cb);
+    [DllImport("pjsipDll.dll")]
+    private static extern int getConfigDataCallback(GetConfigData cb);
+    [DllImport("pjsipDll.dll")]
+    private static extern int onCallHoldConfirmCallback(OnCallHoldConfirm cb);
+    [DllImport("pjsipDll.dll")]
+    private static extern int onMessageReceivedCallback(OnMessageReceivedCallback cb);
+    [DllImport("pjsipDll.dll")]
+    private static extern int onBuddyStatusChangedCallback(OnBuddyStatusChangedCallback cb);
+
+    #endregion Wrapper functions
+
+    #region Variables
+
+    static OnCallStateChanged csDel = new OnCallStateChanged(onCallStateChanged);
+    static OnRegStateChanged rsDel = new OnRegStateChanged(onRegStateChanged);
+    static OnCallIncoming ciDel = new OnCallIncoming(onCallIncoming);
+    //static GetConfigData gdDel = new GetConfigData(getConfigData);
+    static OnCallHoldConfirm chDel = new OnCallHoldConfirm(onCallHoldConfirm);
+    static OnMessageReceivedCallback mrdel = new OnMessageReceivedCallback(onMessageReceived);
+    static OnBuddyStatusChangedCallback bscdel = new OnBuddyStatusChangedCallback(onBuddyStatusChanged);
+
+    private static CCallManager CallManager
+    {
+      get { return CCallManager.getInstance(); }
+    }
+
+    #endregion Variables
+
+    #region Methods
+
+    public int initialize()
+    {
+      // register callbacks (delegates)
+      onCallIncoming( ciDel );
+      onCallStateCallback( csDel );
+      onRegStateCallback( rsDel );
+      onCallHoldConfirmCallback(chDel);
+      onBuddyStatusChangedCallback(bscdel);
+      onMessageReceivedCallback(mrdel);
+
+      // Initialize pjsip...
+      int status = start();
+      return status;
+    }
+
+    public int shutdown()
+    {
+      return dll_shutdown();
+    }
+
+    public int start()
+    {
+      int status = -1;
+      
+      //int port = CallManager.SipPort;
+      int port = 5060;
+      status = dll_init(port);
+      status |= dll_main();
+      return status;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////
+    // Call API
+    //
+    public int registerAccounts()
+    {
+      return registerAccounts(false);
+    }
+
+    public int registerAccounts(bool renew)
+    {
+      if (renew == true)
+      {
+        dll_removeAccounts();
+      } 
+
+      for (int i = 0; i < CallManager.Config.NumOfAccounts; i++)
+      {
+        IAccount acc = CallManager.Config.getAccount(i);
+
+        // reset account state
+        CallManager.setAccountState(i, 0);
+
+        if (acc.Id.Length > 0)
+        {
+          if (acc.HostName == "0") continue;
+
+          string displayName = acc.DisplayName; 
+          // Publish do not work if display name in uri 
+          string uri = displayName + "<sip:" + acc.Id + "@" + acc.HostName + ">";
+          //string uri = "sip:" + Manager.getId(i) + "@" + Manager.getAddress(i) + "";
+          string reguri = "sip:" + acc.HostName; // +":" + CCallManager.getInstance().SipProxyPort;
+
+          string domain = acc.DomainName;
+          string username = acc.UserName;
+          string password = acc.Password;
+
+          dll_registerAccount(uri, reguri, domain, username, password);
+
+          // todo:::check if accId corresponds to account index!!!
+        }
+      }
+      return 1;
+    }
+
+    // Buddy list handling
+    public int addBuddy(string ident)
+    {
+      string uri = "sip:" + ident + "@" + CallManager.Config.getAccount().HostName;
+      return dll_addBuddy(uri, true);
+    }
+
+    public int delBuddy(int buddyId)
+    {
+      return dll_removeBuddy(buddyId);
+    }
+
+    public int sendMessage(string dest, string message)
+    {
+      string uri = "sip:" + dest + "@" + CallManager.Config.getAccount().HostName;
+      return dll_sendMessage(CallManager.Config.DefaultAccountIndex, uri, message);
+    }
+
+    public int setStatus(int accId, EUserStatus status)
+    {
+      return dll_setStatus(accId, (int)status);
+    }
+
+    #endregion Methods
+
+    #region Callbacks
+
+    private static int onCallStateChanged(int callId, int callState)
+    {
+//    PJSIP_INV_STATE_NULL,	    /**< Before INVITE is sent or received  */
+//    PJSIP_INV_STATE_CALLING,	    /**< After INVITE is sent		    */
+//    PJSIP_INV_STATE_INCOMING,	    /**< After INVITE is received.	    */
+//    PJSIP_INV_STATE_EARLY,	    /**< After response with To tag.	    */
+//    PJSIP_INV_STATE_CONNECTING,	    /**< After 2xx is sent/received.	    */
+//    PJSIP_INV_STATE_CONFIRMED,	    /**< After ACK is sent/received.	    */
+//    PJSIP_INV_STATE_DISCONNECTED,   /**< Session is terminated.		    */
+      if (callState == 2) return 0;
+
+      CStateMachine sm = CallManager.getCall(callId);
+      if (sm == null) return 0;
+
+      switch (callState)
+      {
+        case 1:
+          //sm.getState().onCalling();
+          break;
+        case 2:
+          //sm.getState().incomingCall("4444");
+          break;
+        case 3:
+          sm.getState().onAlerting();
+          break;
+        case 4:
+          sm.getState().onConnect();
+          break;
+        case 6:
+          sm.getState().onReleased();
+          break;
+      }
+      return 1;
+    }
+
+    private static int onCallIncoming(int callId, string uri)
+    {
+      string display  = "";
+      string number = "";
+      
+      // get indices
+      int startNum = uri.IndexOf("<sip:");
+      int atPos = uri.IndexOf('@');
+      // search for number
+      if ((startNum > 0)&&(atPos > startNum))
+      {
+        number = uri.Substring(startNum + 5, atPos - startNum - 5);  
+      }
+
+      // extract display name if exists
+      if (startNum >= 0)
+      {
+        display = uri.Remove(startNum).Trim();
+      }
+      else 
+      { 
+        int semiPos = display.IndexOf(';');
+        if (semiPos >= 0)
+        {
+          display = display.Remove(semiPos);
+        }
+        else 
+        {
+          int colPos = display.IndexOf(':');
+          if (colPos >= 0)
+          {
+            display = display.Remove(colPos);
+          }
+        }
+
+      }
+
+      CStateMachine sm = CallManager.createSession(callId, number);
+      sm.getState().incomingCall(number, display);
+      return 1;
+    }
+
+
+    private static int onRegStateChanged(int accId, int regState)
+    {
+      CallManager.setAccountState(accId, regState);
+      return 1;
+    }
+
+
+    private static int onCallHoldConfirm(int callId)
+    {
+      CStateMachine sm = CallManager.getCall(callId);
+      if (sm != null) sm.getState().onHoldConfirm();
+      return 1;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    private static int onMessageReceived(string from, string message)
+    {
+      CallManager.setMessageReceived(from, message);
+      return 1;
+    }
+
+    private static int onBuddyStatusChanged(int buddyId, int status, string text)
+    {
+      CallManager.setBuddyState(buddyId, status, text);
+      return 1;
+    }
+
+    #endregion Callbacks
+
+  }
+
+
+
+} // namespace PjsipWrapper
