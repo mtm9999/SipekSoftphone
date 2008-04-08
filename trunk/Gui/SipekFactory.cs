af2b6af9f8cf4d529671c22a956c051bd0675c83
sasacoh <sasacoh@30a9d3e4-4a32-0410-933f-db000b6b029f>
2008-04-08 07:18:30 +0000

    Bug fix: Buddy presence not subscribed (proxy not assigned) Bug fix: Microphone settings corrections. Todo: add handler for mic balance (now mic volume is handled uniformly)

--- trunk/Gui/SipekFactory.cs
+++ trunk/Gui/SipekFactory.cs
@@ -231,22 +231,6 @@ namespace Sipek
       }
     }
 
-    public bool ImsEnabled
-    {
-      get
-      {
-        short value;
-        if (Int16.TryParse(Properties.Settings.Default.cfgSipAccountImsEnabled[_index], out value))
-        {
-          return value == 1 ? true : false;
-        }
-        return false;
-      }
-      set
-      {
-        Properties.Settings.Default.cfgSipAccountImsEnabled[_index] = (value == true ? "1" : "0");
-      }
-    }
     public string ProxyAddress
     {
       get
